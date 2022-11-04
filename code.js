7be6973784073c4f41eeabd38a0f91a7de616337
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-11-04 17:37:06 -0500

    wth

--- code.js
+++ code.js
@@ -1,11 +1,12 @@
+var vid
 if (window.confirm("attempt to download directly from url?")) {
-  let vid = location.href
+  vid = location.href
 } else {
-  let vid = prompt("video url?")
+  vid = prompt("video url?")
 }
 
-let uri = "https://projectlounge.pw/ytdl/download?url="
+var uri = "https://projectlounge.pw/ytdl/download?url="
 
-let vid = vid // manipulate this with code if you need to // for example: encodeURIComponent(vid) or btoa(vid) - some sites need an encoded video URN for some reason. try downloading a video like normal at first, or reading their api documentation (if they have any)
+//vid = vid // manipulate this with code if you need to // for example: encodeURIComponent(vid) or btoa(vid) - some sites need an encoded video URN for some reason. try downloading a video like normal at first, or reading their api documentation (if they have any)
 
 location = uri + vid
